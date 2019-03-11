d26ccb22b13b65ae8d8fa22f3ca94ef9db67d3d8
LiangYT <381518188@qq.com>
2019-03-11 10:27:21 +0800

    同步！

---
+++ app/src/main/java/com/sinothk/manager/video/demo/VideoManagerMainActivity.java
@@ -0,0 +1,13 @@
+package com.sinothk.manager.video.demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VideoManagerMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video_manager_main);
+    }
+}
